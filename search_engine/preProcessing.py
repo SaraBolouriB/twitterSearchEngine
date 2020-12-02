95a4a00286f476e9fbe71d8dee99d7d9d02129d4
SaraBolouriB <boloorisara@gmail.com>
2020-12-02 18:00:00 +0330

    done first part

--- search_engine/preProcessing.py
+++ search_engine/preProcessing.py
@@ -1,14 +1,21 @@
+import nltk
 import re
+import string
 import preprocessor as p
+
+a = [{"tweetId" : "123", "tweetText" : "Hi (thwer@ m+sy 12 %name)) //======i+s sara **"}]
 
 def preprocess(tweetText):
     tokenizer = nltk.RegexpTokenizer(r"\w+")
 
     for tweet_text in tweetText:
-        cleanTweet = re.sub(r'\d+', '', tweetText["tweetText"]) # Remove Numbers
-        cleanTweet = tweet_text["tweetText"].lower()            # Convert To LowerCase
-        cleanTweet = p.clean(cleanTweet)                        # Remove @, url, ..
+        cleanTweet = re.sub(r'\d+', '', tweet_text["tweetText"])                            # Remove Numbers
+        cleanTweet = cleanTweet.lower()                                                     # Convert To LowerCase
+        cleanTweet = p.clean(cleanTweet)                                                    # Remove urls, mentions
+        cleanTweet = cleanTweet.translate(str.maketrans(dict.fromkeys(string.punctuation))) # Remove (, ), +, %, *, =, /
+        cleanTweet = tokenizer.tokenize(cleanTweet)                                         # Tokenize Each String
         print(f"tweetID {tweet_text['tweetId']}: {cleanTweet}")
 
 
 
+preprocess(a)
