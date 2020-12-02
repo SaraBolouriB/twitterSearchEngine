e1a7f96c8b14639242b1d260aa5433bea2df20a2
SaraBolouriB <boloorisara@gmail.com>
2020-12-02 19:56:10 +0330

    Correct misspelling

--- search_engine/preProcessing.py
+++ search_engine/preProcessing.py
@@ -2,17 +2,17 @@ import nltk
 import re
 import string
 import preprocessor as p
-# from pattern.en import spelling
 from spellchecker import SpellChecker
 
 a = [{"tweetId" : "123", "tweetText" : "Hiiiiiii (thwer@ m+sy 12 %name)) //======i+s sara **"}]
 
 
-# Correcting Word By Reducing Length
+# Reducing Length
 def reduce_lengthening(text):
     pattern = re.compile(r"(.)\1{2,}")
     return pattern.sub(r"\1\1", text)
 
+# Correct Misspelling
 def correction_sent(text):
     spell = SpellChecker()
     splitted_sentence = text.split(" ")[:-1]
@@ -26,14 +26,13 @@ def correction_sent(text):
 def preprocess(tweetText):
     tokenizer = nltk.RegexpTokenizer(r"\w+")
     
-
     for tweet_text in tweetText:
         cleanTweet = re.sub(r'\d+', '', tweet_text["tweetText"])                            # Remove Numbers
         cleanTweet = cleanTweet.lower()                                                     # Convert To LowerCase
         cleanTweet = p.clean(cleanTweet)                                                    # Remove urls, mentions
         cleanTweet = cleanTweet.translate(str.maketrans(dict.fromkeys(string.punctuation))) # Remove (, ), +, %, *, =, /
         cleanTweet = reduce_lengthening(cleanTweet)                                         # Reduce Length
-        cleanTweet = correction_sent(cleanTweet)                                            # Correct misspelling
+        cleanTweet = correction_sent(cleanTweet)                                            # Correct misspellings
         cleanTweet = tokenizer.tokenize(cleanTweet)                                         # Tokenize Each String
         print(f"tweetID {tweet_text['tweetId']}: {cleanTweet}")
 
