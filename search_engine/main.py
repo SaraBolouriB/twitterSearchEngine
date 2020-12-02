4036ef497d68b664ed9b72af7c8de2b26f1baafe
SaraBolouriB <boloorisara@gmail.com>
2020-12-02 20:15:44 +0330

    updated main.py

--- search_engine/main.py
+++ search_engine/main.py
@@ -1,5 +1,5 @@
 from getTextTweets import textTweets
-
+from preProcessing import preprocess
 
 dataset = "../dataset/test.csv"
 
@@ -7,4 +7,5 @@ dataset = "../dataset/test.csv"
 ## the program start here!
 if __name__ == '__main__':
     print("Text of Tweet are below:")
-    textTweets(dataset)
+    allTweetTexts = textTweets(dataset)
+    preprocess(allTweetTexts)
