d93abc6a238dce4e6060c0132502499120dfaad0
SaraBolouriB <boloorisara@gmail.com>
2020-12-10 21:03:23 +0330

    add try except block to profile_info() method

--- search_engine/tweetInformation.py
+++ search_engine/tweetInformation.py
@@ -6,16 +6,20 @@ def profile_info(tweetID):
         Get status object with tweepy API.
         Status Object was used to fetch user profile information.
     '''
-    status = api.get_status(int(tweetID))
-    user_profile = {
-        "user_id" : status.user.id,
-        "user_name" : status.user.name,
-        "screen_name" : status.user.screen_name,
-        "user_location" : status.user.location,
-        "followers_count" : status.user.followers_count,
-        "account_created_at" : status.user.created_at,
-        "tweets_conunt" : status.user.statuses_count
-    }
+    try:
+        status = api.get_status(int(tweetID))
+        user_profile = {
+            "user_id" : status.user.id,
+            "user_name" : status.user.name,
+            "screen_name" : status.user.screen_name,
+            "user_location" : status.user.location,
+            "followers_count" : status.user.followers_count,
+            "account_created_at" : status.user.created_at,
+            "tweets_conunt" : status.user.statuses_count
+        }
+    except:
+        print("The id '" + str(tweetID) + "' does't exist.")
+        user_profile = None
     return(user_profile)
 
 def get_userTweets(username):
