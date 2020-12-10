4b01cb7a4bfaf350f0b841273e35d0186e6098fa
SaraBolouriB <boloorisara@gmail.com>
2020-12-10 13:57:34 +0330

    add header to excel + clean main.py file

--- search_engine/main.py
+++ search_engine/main.py
@@ -3,15 +3,20 @@ from getTextTweets import textTweets
 from preProcessing import preprocess
 
 dataset = "../dataset/test.csv"
-workbook = xlsxwriter.Workbook('cleanTextOutput.xlsx')
-worksheet = workbook.add_worksheet()
-row = 0
-col = 0
 
-## the program start here!
-if __name__ == '__main__':
-    allTweetTexts = textTweets(dataset)
-    cleanTweetText = preprocess(allTweetTexts)
+def print_clean_data(cleanTweetText):
+    '''
+        1. Saved data in excel file wiche was named "cleanTextOutput.xlsx"
+        2. Print clean text
+    '''
+    workbook = xlsxwriter.Workbook('cleanTextOutput.xlsx')
+    worksheet = workbook.add_worksheet()
+    row = 0
+    col = 0
+    worksheet.write(row, col, "Tweet ID")
+    worksheet.write(row, col + 1, "Tweet Text")
+    worksheet.write(row, col + 2, "Cleaned Tweet Text")
+    row += 1
     print("Body of Tweet are below:")
     for tweet in cleanTweetText:
         worksheet.write(row, col, tweet['tweetId'])
@@ -20,3 +25,9 @@ if __name__ == '__main__':
         row += 1
         print(f"TweetID: {tweet['tweetId']}\nOrginalTweet: {tweet['tweetText']}\nCleanTweet: {tweet['tweetClean']}\n=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
     workbook.close()
+
+## the program start here!
+if __name__ == '__main__':
+    allTweetTexts = textTweets(dataset)
+    cleanTweetText = preprocess(allTweetTexts)
+    print_clean_data(cleanTweetText)
