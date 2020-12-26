d1a047819def0c7544a64885fe07d81cf32527b1
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2020-12-26 16:12:14 +0330

    add lost tweet to excel file

--- search_engine/tweetInformation.py
+++ search_engine/tweetInformation.py
@@ -14,7 +14,7 @@ def profile_info():
             status = api.get_status(int(user['tweetId']))
             user_profile.append({"tweet_id": user['tweetId'],"user_id": status.user.id,"user_name": status.user.name,"screen_name": status.user.screen_name,"user_location": status.user.location,"followers_count": status.user.followers_count,"account_created_at": status.user.created_at,"tweets_count": status.user.statuses_count,"favorite_count": status.favorite_count,"retweet_count": status.retweet_count})
         except:
-            print("The id " + str(user['tweetId']) + " does't exist.")
+            user_profile.append({"tweet_id": user['tweetId'],"user_id": "Not Found!","user_name": "Not Found!","screen_name": "Not Found!","user_location": "Not Found!","followers_count": "Not Found!","account_created_at": "Not Found!","tweets_count": "Not Found!","favorite_count": "Not Found!","retweet_count": "Not Found!"})
     return(user_profile)
 
 def get_userTweets(username):
